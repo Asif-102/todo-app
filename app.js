bb0102d6575513c7b3485766f8e6c957d8cf70c5
Asif-102 <aiqbal517@gmail.com>
2022-03-26 19:07:17 +0600

    initial configure complete

---
+++ app.js
@@ -0,0 +1,23 @@
+const lists = [];
+
+const addTodoBtn = document.getElementById("todo-btn");
+const todoInput = document.getElementById("todo-text");
+const todoList = document.getElementById("todo-list");
+const totalItem = document.getElementById("total-item");
+
+addTodoBtn.addEventListener("click", () => {
+    lists.push(todoInput.value);
+    todoInput.value = "";
+    todoList.textContent = "";
+
+    totalItem.innerText = lists.length;
+
+    for(const list of lists)
+    {
+        const li = document.createElement("li");
+        li.setAttribute("class", "list-group-item d-flex justify-content-between");
+        li.innerHTML = `<span>${list}</span><span class="btn btn-light btn-sm">X</span>`
+        todoList.appendChild(li);
+    }
+
+})
